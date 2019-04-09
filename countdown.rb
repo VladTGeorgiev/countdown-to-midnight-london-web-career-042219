e39215619c9dc95fb7944d46d20054dab7515ba4
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-04-09 18:09:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
-
-def countdown
+def countdown(int)
+  
+  counter = 0
+  while counter < int
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    counter += 1
+  end
+  
+  "HAPPY NEW YEAR!"
 end
